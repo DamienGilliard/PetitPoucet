541ce235326883cf7f2ef9e00b76637dd3eab754
DamienGilliard <damiengilliard@outlook.com>
2024-11-08 17:31:58 +0100

    WIP: setting up RTK connection

--- cpp/main.cc
+++ cpp/main.cc
@@ -9,31 +9,45 @@ int main(int argc, char **argv)
     // rtklib's stream server
     static strsvr_t streamServer;
     static volatile int intrflg=0;
+    static char s1[2][MAXSTRPATH]={{0}};
+    static char s2[2][MAXSTRPATH]={{0}};
+    static char cmd_strs[2][MAXRCVCMD]={"",""};
+    static char cmd_periodic_strs[2][MAXRCVCMD]={"",""};
     int options[]={10000,10000,2000,32768,10,0,30,0}; //not quite sure what this is... but it is needed
-    char *msg, *opt;
+    char *msg, *opt, *local, *proxy;
     int types[2] = {STR_NTRIPCLI, STR_SERIAL};
+    int formats[2] = {-1, -1};
     double stapos[3]={0};
-    char *paths[2];
-    char *logs[2];
+    char *paths[2] = {0};
+    char *logs[2] = {0};
     char *cmds[2];
     char *cmds_periodic[2];
 
     //dummy message and option
     msg = new char[1];
     opt = new char[1];
+    local = new char[1];
+    proxy = new char[1];
     *cmds = {new char[0]};
     *cmds_periodic = {new char[0]};
 
     *msg = ' ';
     *opt = ' ';
+    *local = ' ';
+    *proxy = ' ';
 
-    // rtklib's stream converter
     strconv_t *converter[1]={NULL};
-    if (!(converter[1] = strconvnew(0, 0, msg, 0, 1, opt))){
-        std::cout<<"Help, converstion of stream converter failed !!" << std::endl;
+
+    for (int i=0;i<2;i++) {
+        paths[i]=s1[i];
+        logs[i]=s2[i];
+        cmds[i]=cmd_strs[i];
+        cmds_periodic[i]=cmd_periodic_strs[i];
     }
 
     strsvrinit(&streamServer,2);
+    strsetdir(local);
+    strsetproxy(proxy);
 
     // Reading the config file
     std::shared_ptr<petitpoucet::filemanipulation::ConfigurationSetup> setup = std::make_shared<petitpoucet::filemanipulation::ConfigurationSetup>();
