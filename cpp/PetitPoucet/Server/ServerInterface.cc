66bf6a293b6d8ddcd02992b5d0e91d278f1c733f
DamienGilliard <damiengilliard@outlook.com>
2024-11-22 23:19:39 +0100

    ADD: PPServer Class

---
+++ cpp/PetitPoucet/Server/ServerInterface.cc
@@ -0,0 +1,219 @@
+#include "ServerInterface.hh"
+
+namespace petitpoucet::serverinterface
+{
+   void ConfigurationSetup::ReadConfigFile(std::string configFileName, std::string *casterName, std::string *serialPortName)
+    {
+        std::ifstream configFile(configFileName);
+        std::string line;
+        if (configFile.is_open())
+        {
+            while (getline(configFile, line))
+            {
+                // avoid comment lines
+                char firstChar = line[0];
+                if (&firstChar == "#"){continue;};
+                
+                int stringLength = line.size();
+                int index = line.find(":");
+                std::string flag = line.substr(0, index);
+                if(flag == "caster"){*casterName = line.substr(index+2, stringLength);};
+                if(flag == "serial"){*serialPortName = line.substr(index+2, stringLength);};
+            } 
+            configFile.close();
+        }
+        else
+        {
+            std::cout << "The config file 'config.txt' could not be read";
+        }
+    }
+
+    void PPServer::ReadGNGGASolution(double &longitude, double &latitude, double &altitude, int &signalToNoiseRatio, double &horizontalDilutionOfPrecision, std::string &fixQuality, int &timeStamp)
+    {
+        std::string line;
+        std::stringstream stringStreamedBuffer;
+        stringStreamedBuffer << this->RTKLIBStreamServer.buff;
+        int i = 0;
+        char delimiter = ',';
+        std::string item;
+        std::vector<int> signalToNoiseRatios;
+        int meanSignalToNoiseRatio;
+        while(std::getline(stringStreamedBuffer, line))
+        {
+            std::vector<std::string> vectorizedGNSSMessage;
+            if (line.rfind("$GNGGA", 0) == 0) 
+            {
+                std::stringstream stringStreamedLine(line);
+                while (std::getline(stringStreamedLine, item, delimiter))
+                {
+                    vectorizedGNSSMessage.push_back(item);
+                }
+                longitude = std::stold(vectorizedGNSSMessage[4]);
+                latitude = std::stold(vectorizedGNSSMessage[2]);
+                altitude = std::stold(vectorizedGNSSMessage[9]);
+                timeStamp = std::stold(vectorizedGNSSMessage[1]);
+                if (vectorizedGNSSMessage[6] == "5")
+                {
+                    fixQuality = "| RTK fixed ! :)";
+                }
+                else if (vectorizedGNSSMessage[6] == "4")
+                {
+                    fixQuality = "| RTK float !";
+                }
+                else if (vectorizedGNSSMessage[6] == "2")
+                {
+                    fixQuality = "| Differential GPS fix";
+                }
+                else if (vectorizedGNSSMessage[6] == "1")
+                {
+                    fixQuality = "| GPS fix :/";
+                }
+                else if (vectorizedGNSSMessage[6] == "0")
+                {
+                    fixQuality = "| Invalid fix :(";
+                }
+                horizontalDilutionOfPrecision = std::stold(vectorizedGNSSMessage[8]);
+            }
+            
+            if (line.rfind("$GPGSV", 0) == 0||line.rfind("$GLGSV", 0) == 0||line.rfind("$GAGSV", 0) == 0|| line.rfind("$GBGSV", 0) == 0)
+            {
+                std::stringstream stringStreamedLine(line);
+                while (std::getline(stringStreamedLine, item, delimiter))
+                {
+                    vectorizedGNSSMessage.push_back(item);
+                }
+                if(!vectorizedGNSSMessage[7].length() == 0)
+                {
+                    signalToNoiseRatios.push_back(std::stoi(vectorizedGNSSMessage[7]));
+                }
+            }
+            if(signalToNoiseRatios.size()>0)
+            {
+                for(int i=0; i<signalToNoiseRatios.size(); i++)
+                {
+                    meanSignalToNoiseRatio += signalToNoiseRatios[i];
+                }
+                meanSignalToNoiseRatio /= signalToNoiseRatios.size();
+                signalToNoiseRatio = meanSignalToNoiseRatio;
+            }
+            
+        }
+    }
+
+    PPServer PPServer::SetupCorrectionServer(std::string *casterName, std::string *serialPortName, PPServerOptions options)
+    {
+        PPServer CorrectionServer;
+
+        static char s1[3][MAXSTRPATH]={{0}};
+        static char s2[3][MAXSTRPATH]={{0}};
+        static char cmd_strs[3][MAXRCVCMD]={"",""};
+        static char cmd_periodic_strs[3][MAXRCVCMD]={"",""};
+        char *paths[3] = {0};
+        int types[3] = {STR_NTRIPCLI, STR_SERIAL};
+        char *logs[3] = {0};
+        strconv_t *converter[3] = {NULL};
+        char* commands[3] = {0};
+        char* commandsPeriodic[3] = {0};
+        double stapos[3] = {0};
+
+        for (int i=0;i<3;i++) 
+        {
+            paths[i]=s1[i];
+            logs[i]=s2[i];
+            commands[i]=cmd_strs[i];
+            commandsPeriodic[i]=cmd_periodic_strs[i];
+        }
+        paths[0] = new char[casterName->size() + 1];
+        paths[1] = new char[serialPortName->size() + 1];
+
+        strcpy(paths[0], casterName->c_str());
+        strcpy(paths[1], serialPortName->c_str());
+
+        strsvrinit(&CorrectionServer.RTKLIBStreamServer, 2);
+        strsetdir("");
+        strsetproxy("");
+
+        if (! strsvrstart(&CorrectionServer.RTKLIBStreamServer, 
+                          options.ToIntegers(), 
+                          types,
+                          (const char **)paths,
+                          (const char **)logs,
+                          converter,
+                          (const char **)commands,
+                          (const char **)commandsPeriodic,
+                          stapos))
+        {
+            std::cerr << "Correction server start failed" << std::endl;
+        }
+        
+        return CorrectionServer;
+    }
+
+    PPServer PPServer::SetupReaderServer(std::string *serialPortName, PPServerOptions options)
+    {
+        PPServer ReaderServer;
+
+        static char s1[3][MAXSTRPATH]={{0}};
+        static char s2[3][MAXSTRPATH]={{0}};
+        static char cmd_strs[3][MAXRCVCMD]={"",""};
+        static char cmd_periodic_strs[3][MAXRCVCMD]={"",""};
+        char *paths[3] = {0};
+        int types[3] = {STR_SERIAL, STR_NONE};
+        char *logs[3] = {0};
+        strconv_t *converter[3] = {NULL};
+        char* commands[3] = {0};
+        char* commandsPeriodic[3] = {0};
+        double stapos[3] = {0};
+
+        for (int i=0;i<3;i++) 
+        {
+            paths[i]=s1[i];
+            logs[i]=s2[i];
+            commands[i]=cmd_strs[i];
+            commandsPeriodic[i]=cmd_periodic_strs[i];
+        }
+        paths[0] = new char[serialPortName->size() + 1];
+
+        strcpy(paths[0], serialPortName->c_str());
+
+        strsvrinit(&ReaderServer.RTKLIBStreamServer, 2);
+        strsetdir("");
+        strsetproxy("");
+
+        if (! strsvrstart(&ReaderServer.RTKLIBStreamServer, 
+                          options.ToIntegers(), 
+                          types,
+                          (const char **)paths,
+                          (const char **)logs,
+                          converter,
+                          (const char **)commands,
+                          (const char **)commandsPeriodic,
+                          stapos))
+        {
+            std::cerr << "Reader server start failed" << std::endl;
+        }
+        
+        return ReaderServer;
+    }
+
+    void PPServer::GetServerStatus(int *stat, int *log_stat, int *byte, int *bps, std::string *stringMessage)
+    {
+        char charMessage[1024] ="";
+        strsvrstat(&this->RTKLIBStreamServer, stat,log_stat, byte, bps, charMessage);
+        *stringMessage = std::string(charMessage);
+    }
+
+    void PPServer::GetCurrentSolution(double &longitude, double &latitude, double &altitude, int &signalToNoiseRatio, int &timeStamp)
+    {
+        double horizontalDilutionOfPrecision;
+        std::string fixQuality;
+
+        ReadGNGGASolution(longitude, 
+                          latitude, 
+                          altitude, 
+                          signalToNoiseRatio, 
+                          horizontalDilutionOfPrecision, 
+                          fixQuality, 
+                          timeStamp);
+    }
+}
---
+++ cpp/PetitPoucet/Server/ServerInterface.hh
@@ -0,0 +1,128 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <array>
+#include <sstream>
+#include <vector>
+
+#include <rtklib.h>
+
+namespace petitpoucet::serverinterface
+{
+    /*
+    @brief This class stores the method for reading the confifile
+    */
+    class ConfigurationSetup
+    {
+        // Constructors
+        public:
+            ConfigurationSetup(){}
+            ~ConfigurationSetup() = default;
+
+        // Methods
+        public:
+            /*
+            @brief Reads a configuration files and attibutes values to a caster name and a serial port name.
+            @param std::string configFilName The file name where the configration is stored
+            @param std::string* casterName the pointer to the caster name variable where the name of the caster stored in the config file will be stored
+            @param std::string* serialPortName the pointer to the serial port name variable where the name of the serial port stored in the config file will be stored
+            */
+            void ReadConfigFile(std::string configFileName, std::string *casterName, std::string *serialPortName);
+
+    };
+
+    /*
+    @brief This class stores the options for the PPServer when setup by PPServerInterface::SetupServer
+    */
+    class PPServerOptions
+    {
+        //Constructor
+        public:
+        PPServerOptions(){}
+        ~PPServerOptions(){};
+
+        // Properties
+        public:
+        int inactiveTimeout = 10000; // 10 000 ms
+        int intervalToReconnect = 10000; // 10 000 ms
+        int averagingTimeOfDataRate = 1000; // 1000 ms
+        int bufferSize = 32768; // 32768 bytes
+        int serverCycle = 10; // 10ms
+        int NMEARequestCycle = 0; // 0ms = no NMEA requests
+        int fileSwapMargin = 30; // 30s
+        int relayBackToOutputstream = 0; // 0 =  no relay back
+
+        //Methods
+        public:
+        int* ToIntegers()
+        {
+            int* options = new int[8]{this->inactiveTimeout,
+                                    this->intervalToReconnect,
+                                    this->averagingTimeOfDataRate,
+                                    this->bufferSize,
+                                    this->serverCycle,
+                                    this->NMEARequestCycle,
+                                    this->fileSwapMargin,
+                                    this->relayBackToOutputstream};
+            return options; // Be careful, caller must delete this pointer
+        };
+    };
+
+    class PPServer
+    {
+        // Constructor
+        private:
+        PPServer(){}
+        public:
+        ~PPServer(){};
+
+        // Properties
+        private:
+        /*
+        @brief The rtklib streamserver running in the background
+        */
+        inline static strsvr_t RTKLIBStreamServer;
+
+        // Methods
+        private:
+        void ReadGNGGASolution(double &longitude, double &latitude, double &altitude, int &signalToNoiseRatio, double &horizontalDilutionOfPrecision, std::string &fixQuality, int &timeStamp);
+        public:
+        /*
+        @brief Sets a server up for correcting and reading corrected data
+        */
+        static PPServer SetupCorrectedReaderServer(std::string *casterName, std::string *serialPortName, PPServerOptions options);
+
+        /*
+        @brief Sets a server up to send correction data to an RTK reciever, and returns it as a Server object.
+        @param std::string* casterName the pointer to the name of the caster we use for the RTK
+        @param std::string* serialPortName the pointer to the name of the serial port where the RTK reciever is connected
+        @param PPServerOptions options The options for the server
+        @return PPServer a running server that sends the correction data to the reciever
+        */
+        static PPServer SetupCorrectionServer(std::string *casterName, std::string *serialPortName, PPServerOptions options);
+
+        /*
+        @brief Sets a server up to listen to an RTK reciever, and returns it as a Server object.
+        @param std::string* serialPortName the pointer to the name of the serial port where the RTK reciever is connected
+        @param PPServerOptions options The options for the server
+        @return PPServer a running server that reads the  data from the reciever
+        */
+        static PPServer SetupReaderServer(std::string *serialPortName, PPServerOptions options);
+
+        /*
+        @brief Gets the status of the server and stores it in pointers given as parameters*/
+        void GetServerStatus(int *stat, int *log_stat, int *byte, int *bps, std::string *stringMessage);
+
+        void GetCurrentSolution(double &longitude, double &latitude, double &altitude, int &signalToNoiseRatio, int &timeStamp);
+
+        void GetCurrentSolutionQuality(int *signalToNoiseRatio, double *horizontalDilutionOfPrecision, std::string *fixQuality);
+    };
+
+    class PPServerInterface
+    {
+        // Constructor
+        public:
+        PPServerInterface(){}
+        ~PPServerInterface(){};
+    };
+}
