fcdc102b7e33fc7604d9b608cc50a5b825e966f7
DamienGilliard <DamienGilliard@users.noreply.github.com>
2025-06-10 18:24:42 +0000

    deploy: a8445a606d169e58a7131ab042d539604d31e03d

--- search/all_d.js
+++ search/all_d.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['utils_2ecc_0',['Utils.cc',['../Utils_8cc.html',1,'']]],
-  ['utils_2ehh_1',['Utils.hh',['../Utils_8hh.html',1,'']]]
+  ['usage_0',['Usage',['../index.html#autotoc_md2',1,'']]],
+  ['utils_2ecc_1',['Utils.cc',['../Utils_8cc.html',1,'']]],
+  ['utils_2ehh_2',['Utils.hh',['../Utils_8hh.html',1,'']]]
 ];
