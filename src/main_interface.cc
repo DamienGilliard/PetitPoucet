3db8d45417dd40bec962704b5ddb2b683cfd51db
Damien Gilliard <damien.gilliard@epfl.ch>
2025-06-29 17:34:44 +0200

    feat: change executable names in project for more explicit naming

---
+++ src/main_interface.cc
@@ -0,0 +1,101 @@
+#include "rtklib.h"
+#include <iostream>
+#include <fstream>
+#include <memory>
+#include "PetitPoucet.hh"
+#include <chrono>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+void runCorrectionServer()
+    {
+        execl("./correction_server", "./correction_server", NULL);
+        std::cerr << "execl() failed!\n";
+    }
+
+int main(int argc, char **argv)
+{
+    std::string configFileName = "../config.txt";
+    std::string casterName;
+    std::string serialPortName, serialPortNamecopy;
+    std::string readConfigFileMessage = "Do you want to use a config file or define the parameters yourself?";
+    int readConfigFile = petitpoucet::ui::giveChoiceTwoOptions("Read from config file", " Define myself at runtime", readConfigFileMessage);
+    std::vector<std::string> labels;
+    int positionWithLabels, recordingTime = 0;
+
+    if(readConfigFile == 0)
+    {
+        std::shared_ptr<petitpoucet::utils::filemanipulation::ConfigurationSetup> setup = std::make_shared<petitpoucet::utils::filemanipulation::ConfigurationSetup>();
+        setup->ReadConfigFile(configFileName, &casterName, &serialPortName, &labels);
+    }
+    else if (readConfigFile == 1)
+    {
+        petitpoucet::ui::configfromUserInput(casterName, serialPortName);
+    }
+
+    std::string messageForInstantaneous = "Do you want to get instantaneous position or position over time?";
+    int overTime = petitpoucet::ui::giveChoiceTwoOptions("Instantaneous", "Over time", messageForInstantaneous);
+    if(overTime == 0)
+    {
+        std::cout << "You chose to get instantaneous position.\n";
+    }
+    else
+    {
+        std::string messageForRecordingTime = "For how long do you want to record the position?";
+        recordingTime = petitpoucet::ui::giveChoiceMultipleOptions({"30s", "60s", "120s", "5min"}, messageForRecordingTime);
+        if(recordingTime == 0)
+        {
+            recordingTime = 30;
+        }
+        else if(recordingTime == 1)
+        {
+            recordingTime = 60;
+        }
+        else if(recordingTime == 2)
+        {
+            recordingTime = 120;
+        }
+        else if(recordingTime == 3)
+        {
+            recordingTime = 300;
+        }
+
+        std::string messageForLabels = "Do you want to associate position with labels ?";
+        positionWithLabels = petitpoucet::ui::giveChoiceTwoOptions("associate position with labels", "do not associate position with labels", messageForLabels);
+    }
+
+    petitpoucet::serverinterface::PPServerOptions options;
+    petitpoucet::serverinterface::CoordinateSystem coordinateSystem = petitpoucet::serverinterface::CoordinateSystem::WGSDecimals;
+
+    pid_t pid = fork();
+
+    if(pid < 0) 
+    {
+        std::cerr << "Fork failed!\n";
+    }
+    if(pid == 0)
+    {
+        runCorrectionServer();
+    }
+    else
+    {
+        if(!overTime)
+        {
+            petitpoucet::ui::interfaceForInstantaneousPosition(30, options, casterName, serialPortName, coordinateSystem);
+        }
+        else
+        {
+            if (positionWithLabels == 0)
+            {
+                petitpoucet::ui::interfaceForPositionOverTimeWithLabelsAndTimer(30, options, casterName, serialPortName, coordinateSystem, std::chrono::seconds(recordingTime), labels);
+            }
+            else
+            {
+                petitpoucet::ui::interfaceForPositionOverTime(30, options, casterName, serialPortName, coordinateSystem);
+            }
+        }
+        kill(pid, SIGTERM);
+    }
+    return 0;
+}
